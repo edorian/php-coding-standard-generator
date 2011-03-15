b8d86bae0ea4e72f9f2e5c76be4198f9d3aaf9ec
Volker Dusch <github@wallbash.com>
2011-03-15 10:11:20 +0100

    Refactor: Made a proper object out of the first functions

--- website/js/phpmd.js
+++ website/js/phpmd.js
@@ -1,33 +1,50 @@
+var pcsg = pcsg || {};
 
-function phpmd_init(into) {
+pcsg.Phpmd = (function() {
 
-    $.ajax({
-        type: "GET",
-        url: "resources/phpmd/rulesets.json",
-        dataType: "json",
-        success: function(data) {
-            $.each(data, function() {
-                into.append('<h2>' + this + '</h2>');
-                phpmd_file(this, into);
-            })
+    var members = {
+        "container": null
+    };
+
+    var methods = (function() {
+        return {
+            add: function() {
+                return data.a + data.b;
+            },
+            renderFile: function(file) {
+                $.ajax({
+                    type: "GET",
+                    url: "resources/phpmd/" + file,
+                    dataType: "xml",
+                    async: false,
+                    success: function(xml) {
+                        $(xml).find('ruleset description ').each(function() {;
+                            members.container.append("<pre>" + $(this).text() + "</pre>");
+                        })
+                    }
+                });
+            }
         }
-    });
+    })();
 
-}
-
-function phpmd_file(file, into) {
-
-    $.ajax({
-        type: "GET",
-        url: "resources/phpmd/" + file,
-        dataType: "xml",
-        async: false,
-        success: function(xml) {
-            $(xml).find('ruleset description').each(function() {;
-                into.append("<pre>" + $(this).text() + "</pre>");
-            })
+    // public 
+    return {
+        init: function(container) {
+            members.container = container;
+        },
+        render: function() {
+            $.ajax({
+                type: "GET",
+                url: "resources/phpmd/rulesets.json",
+                dataType: "json",
+                success: function(data) {
+                    $.each(data, function() {
+                        members.container.append('<h2>' + this + '</h2>');
+                        methods.renderFile(this);
+                    })
+                }
+            });
         }
-    });
-
-}
+    }
+})();
 
