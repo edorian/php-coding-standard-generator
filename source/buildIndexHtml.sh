d52f50af5ee1e70f67c75e183433ec93c7ff9c55
Volker Dusch <github@wallbash.com>
2012-01-24 17:26:45 +0100

    Cleanup after refactoring

--- source/buildIndexHtml.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -e "/{phpmd}/r phpmd.html" -e "/{phpmd}/d" index.html.template > ../website/index.html
--- source/buildPHPMDhtml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILES=../externals/phpmd/src/main/resources/rulesets/*
-for file in $FILES
-do
-    filename=`basename $file`
-    xsltproc --stringparam file $filename --stringparam tool phpmd ruleset.xslt $file >> phpmd.html
-done
-
